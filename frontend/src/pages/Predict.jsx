a3748e0827729e557fd5bd61d210c90d7a8e7021
sathvikms1720-droid <sathvikms1720@gmail.com>
2026-06-09 14:13:36 +0530

    Connected frontend to deployed backend

--- frontend/src/pages/Predict.jsx
+++ frontend/src/pages/Predict.jsx
@@ -211,7 +211,7 @@ function Predict() {
       );
 
       const response = await axios.post(
-        "http://127.0.0.1:5000/predict",
+        "https://disease-prediction-api-dw67.onrender.com/predict",
         {
           symptoms: backendSymptoms,
         }
